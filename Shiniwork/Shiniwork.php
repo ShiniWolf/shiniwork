88463391164659463e2fc1c8185d9eada503b08c
Philippe Kevin <kevin.philippe06@gmail.com>
2016-03-05 15:43:07 +0100

    + Slim twig view added

--- Shiniwork/Shiniwork.php
+++ Shiniwork/Shiniwork.php
@@ -4,6 +4,8 @@
 
 
     use Slim\App;
+    use Slim\Views\Twig;
+    use Slim\Views\TwigExtension;
 
     class Shiniwork extends App
     {
@@ -13,5 +15,28 @@
 
             $container = new Settings();
             parent::__construct($container);
+
+            $this->registerView();
+        }
+
+        protected function registerView ()
+        {
+            $container = $this->getContainer();
+            $settings  = $container->settings;
+
+            if (!empty($settings['view']) && !empty($settings['view']['twig'])) {
+                $container['view'] = function ($c) {
+                    $view = new Twig($c->settings['view']['template_path'], [
+                        'cache' => $c->settings['view']['twig']['cache']
+                    ]);
+
+                    $view->addExtension(new TwigExtension($c['router'], $c['request']->getUri()));
+
+                    return $view;
+                };
+            }
+            else {
+                throw new \Exception('Key "view" and "view[twig]" not found in config file');
+            }
         }
     }
